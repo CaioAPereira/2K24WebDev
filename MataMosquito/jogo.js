a8f8034ed9750716dccd3c8f60b5363be79bc085
Caio A. Pereira <130467755+CaioAPereira@users.noreply.github.com>
2024-10-09 08:40:16 -0300

    Add files via upload

---
+++ MataMosquito/jogo.js
@@ -0,0 +1,63 @@
+
+var altura = 0
+var largura = 0
+
+function ajustaTamanhoPalcoJogo() {
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+    console.log(altura, largura)
+}
+
+ajustaTamanhoPalcoJogo()
+
+function posicaoRandomica() {
+
+    // Remover mosquito anterior, pelo id, caso já exista
+    if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+    }
+    
+
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX // Se posição < 0 recebe 0 senão recebe x
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+    //Criar o elemento HTML
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosca.png'
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio();
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+
+    document.body.appendChild(mosquito)
+
+}
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+
+    switch (classe) {
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
+}
+
+function ladoAleatorio(){
+    var classe = Math.floor(Math.random() * 2)
+
+    switch (classe) {
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+    }
+
+}
